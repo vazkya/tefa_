86c848515e3b808fc35c5ddf8757761d7af80b91
vazkya <142280637+vazkya@users.noreply.github.com>
2024-02-26 16:06:02 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row p-2 bg-primary text-white">
+        <div class="col-lg-1">
+            <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN 4">
+        </div>
+
+        <div class="col-lg-11">
+            <h1>Perpus Digital</h1>
+            <address>
+                SMKN 4 Tasikmlaya<br>
+                Jl.Depok, Sukamenak, Purbaratu
+            </address>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+img {
+    height: 140px;
+    width: 125px;
+}
+</style>
