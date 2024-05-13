9dbf44897055b92db286a1a8cd59dd2d1510febf
vazkya <142280637+vazkya@users.noreply.github.com>
2024-05-13 14:17:47 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
+  modules:["@nuxtjs/supabase"],
+  supabase: {redirect: false},
 })
