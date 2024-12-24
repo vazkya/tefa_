618159fb89037908ded596ccf1ffd0a7e7f9191f
vazkya <142280637+vazkya@users.noreply.github.com>
2024-12-24 18:00:57 +0700

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,59 +1,41 @@
 <template>
-    <div>
-      <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg bg-primary navbar-dark" style="margin-bottom: 40px;">
-            <div class="container">
-            <!-- Logo dan Brand -->
-                    <img src="~/assets/img/logo.png" alt="Logo Sekolah" width="30" height="35" class="me-2">
-                    <div style="color: white;">SMKN 4 TASIKMALAYA</div>
-    
-            <!-- Tombol Toggler untuk Mobile -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <!-- Menu Navbar -->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" to="./">BERANDA</nuxt-link>
-                    </li>
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" to="./profil">PROFIL</nuxt-link>
-                    </li>
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" to="./data">DATA</nuxt-link>
-                    </li>
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" to="./jurusan">JURUSAN</nuxt-link>
-                    </li>
-                    <li class="nav-item">
-                        <nuxt-link class="nav-link" to="./eskul">EKSTRAKURIKULER</nuxt-link>
-                    </li>
-                </ul>
-            </div>
-            </div>
-        </nav>
-  
-        <!-- Konten Halaman -->
-        <nuxt />
+  <div class="row p-3 bg-primary text-white align-items-center">
+    <!-- Logo SMK -->
+    <div class="col-lg-1 text-center">
+      <img src="~/assets/img/LOGO-SMK4.webp" alt="Logo SMKN 4" />
     </div>
+
+    <!-- Informasi Perpus -->
+    <div class="col-lg-11">
+      <h1 class="title">PERPUS DIGITAL</h1>
+      <address class="address">
+        SMKN 4 Tasikmalaya<br />
+        Jl. Depok, Sukamenak, Purbakarta
+      </address>
+    </div>
+  </div>
 </template>
-  
-  <script>
-  export default {
-    name: 'DefaultLayout'
-  }
-  </script>
-  
-  <style scoped>
-  /* Menambahkan padding pada navbar */
-  .navbar {
-    padding: 10px 15px;
-  }
-  
-  /* Styling untuk logo */
-  .navbar-brand img {
-    max-height: 35px;
-  }
-  </style>
+
+<style scoped>
+/* Gaya untuk logo */
+img {
+  margin-top: 5px;
+  height: 130px;
+  width: 110px;
+}
+
+/* Gaya untuk judul */
+.title {
+  margin-top: 10px;
+  margin-left: 10px;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+/* Gaya untuk alamat */
+.address {
+  margin-left: 10px;
+  font-size: 1rem;
+  line-height: 1.5;
+}
+</style>
